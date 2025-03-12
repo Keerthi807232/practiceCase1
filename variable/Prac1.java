f5a87ba54e59aef4042efb7072e890bbcf0baa37
KishoreP108 <kishorekishore@gmail.com>
2025-03-12 13:59:44 +0530

    Addition

---
+++ variable/Prac1.java
@@ -0,0 +1,13 @@
+public class Prac1{
+    public void add(int x,int y){
+        int z=x+y;
+        System.out.println(z);
+    }
+    public static void main(String args[]){
+        int x=10;
+        int y=20;
+        Prac1 obj=new Prac1();
+        obj.add(x,y);
+
+    }
+}
