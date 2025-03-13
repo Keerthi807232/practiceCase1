4bcd604a7ae87a59b2873b2628a3ed8962a60547
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 14:46:22 +0530

    Counting leap years

---
+++ Countleapyear/Cleap.java
@@ -0,0 +1,10 @@
+public class Cleap{
+    public static void main(String args[]){
+        int count=0;
+        for(int i=2020;i<2030;i++)
+        if(i%4==0){
+            count=count+1;
+        }
+        System.out.println(count);
+    }
+}
