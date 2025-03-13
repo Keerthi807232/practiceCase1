31c8abd3bd371ba06639d907b39e938970df3767
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 11:07:10 +0530

    Checking age

---
+++ MajorMinorFinder/Pro1.java
@@ -0,0 +1,11 @@
+public class Pro1{
+    public static void main(String args[]){
+        int Age=19;
+        if(Age>=18){
+            System.out.println("She or He is Major");
+        }
+        if(Age<18){
+            System.out.println("She or He is Minor");
+        }
+    }
+}
