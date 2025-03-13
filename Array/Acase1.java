e9ca88f8d3296ca09e7b36a9d1e1f670a1df9f4b
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 12:12:58 +0530

    Array problems

---
+++ Array/Acase1.java
@@ -0,0 +1,6 @@
+public class Acase1{
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5};
+        System.out.println(a[0]);
+    }
+}
---
+++ Array/Acase2.java
@@ -0,0 +1,6 @@
+public class Acase2{
+    public static void main(String args[]){
+        String Animals[]={"cat","dog","cow","goat"};
+        System.out.println(Animals[2]);
+    }
+}
