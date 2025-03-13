310b056166fef87138098ebca23b1ed03b6e176b
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 15:24:38 +0530

    Addition of dynamic

---
+++ Dynamicaddition/Dadd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Dadd{
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of x:");
+        int x=sc.nextInt();
+        System.out.println("Enter the value of y:");
+        int y=sc.nextInt();
+        int z=x+y;
+        System.out.println("The addition of a and y:"+z);
+
+    }
+}
