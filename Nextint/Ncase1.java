6ea503844196a346fd31749e30440ad2679f0e9c
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 15:09:07 +0530

    Nextint type for age

---
+++ Nextint/Ncase1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Ncase1{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter your Age");
+        int Age=input.nextInt();
+        System.out.println(Age);
+    }
+}
