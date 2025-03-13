3229dcb43b3beeb6ac9d6dd975c5be7c3b9ab4c2
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 10:49:34 +0530

    Conditonal Statement Performance

---
+++ ConditionalStatement/State1.java
@@ -0,0 +1,10 @@
+public class State1{
+    public static void main(String args[]){
+        if(true){
+            System.out.println(25>15);
+        }
+        if(false){
+            System.out.println(25<15);
+        }
+    }
+}
