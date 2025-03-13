26cc332fa9733c3d39e91e91de6e8a23eb57557e
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 10:22:45 +0530

    Arithmetic operations

---
+++ ArithmeticOperator/Operation.java
@@ -0,0 +1,9 @@
+public class Operation{
+    public static void main(String args[]){
+        System.out.println(20+30);
+        System.out.println(30-15);
+        System.out.println(20*3);
+        System.out.println(50/5);
+        System.out.println(20%5);
+    }
+}
