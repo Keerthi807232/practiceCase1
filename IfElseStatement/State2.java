b9e406fd5b51a61fae2d09f080771f1759b21059
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 11:00:00 +0530

    If-Else statement satisfication

---
+++ IfElseStatement/State2.java
@@ -0,0 +1,10 @@
+public class State2{
+    public static void main(String args[]){
+        if(true){
+            System.out.println(12<3);
+        }
+        else{
+            System.out.println("Notsatisfies");
+        }
+    }
+}
