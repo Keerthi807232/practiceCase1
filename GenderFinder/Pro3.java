3cbf3f46fdb8f82c993ce38444345acec79744be
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 11:34:14 +0530

    Checking person gender

---
+++ GenderFinder/Pro3.java
@@ -0,0 +1,13 @@
+public class Pro3{
+    public static void main(String args[]){
+        String Gender="F";
+        if(Gender=="M"){
+            System.out.println("The person is Male");
+        }
+        if(Gender=="F")
+        {
+            System.out.println("The person is Female");
+        }
+
+    }
+}
