22f75b8b75d3b6d1f618ef95755214cc102c0cab
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 14:23:05 +0530

    Leap year

---
+++ Leapyear/Lcase.java
@@ -0,0 +1,11 @@
+public class Lcase{
+    public static void main(String args[]){
+        int i=2020;
+        if(i%4==0){
+            System.out.println("Leap yeaR");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
