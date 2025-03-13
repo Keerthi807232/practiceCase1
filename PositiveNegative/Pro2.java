4f161304aa8739f4c9298289f93d14de0aa34d8c
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 11:11:02 +0530

    Checking Numbers as negative or positive

---
+++ PositiveNegative/Pro2.java
@@ -0,0 +1,11 @@
+public class Pro2{
+    public static void main(String args[]){
+        int number=-11;
+        if(number<=1){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Positive");
+        }
+    }
+}
