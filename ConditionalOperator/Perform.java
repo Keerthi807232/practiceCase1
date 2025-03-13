e5bbc15a83d9e6beefdbf2f2bec24eb658c36d13
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-13 10:32:43 +0530

    Conditional opertion performance

---
+++ ConditionalOperator/Perform.java
@@ -0,0 +1,10 @@
+public class Perform{
+    public static void main(String args[]){
+        System.out.println(20<30);
+        System.out.println(30>15);
+        System.out.println(20>=3);
+        System.out.println(50<=5);
+        System.out.println(5==5);
+        System.out.println(6!=5);
+    }
+}
