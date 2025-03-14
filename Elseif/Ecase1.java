9c999efb8b198edcfdc739db139b12946159536d
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-14 09:36:29 +0530

    Else if statements

---
+++ Elseif/Ecase1.java
@@ -0,0 +1,14 @@
+public class Ecase1{
+    public static void main(String args[]){
+        int num=0;
+        if(num<=-1){
+            System.out.println("Negative");
+        }
+        else if(num==0){
+            System.out.println("Neutral number");
+        }
+        else{
+            System.out.println("Positive");
+        }
+    }
+}
