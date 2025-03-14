67ba1b3b8cceb31b3527a8bd51f9d0e9acdee1da
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-14 10:25:48 +0530

    Getting input from the user of n value

---
+++ Nnumbers/Ncase1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ncase1{
+    public static void main(String args[]){
+        int i=0;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the number ");
+        int n=input.nextInt();
+        while(i<n){
+        i=i+1;
+        System.out.println(i);
+        }
+
+    }
+}
