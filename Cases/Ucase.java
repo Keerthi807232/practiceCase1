b411a1b48e3959c03e2b74895745c17edf581bbb
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-14 12:03:31 +0530

    Cases

---
+++ Cases/Ucase.java
@@ -0,0 +1,8 @@
+public class Ucase{
+    public static void main(String args[])
+    {
+        String x="hello Programmer";
+        System.out.println(x.toUpperCase());
+        System.out.println(x.toLowerCase());
+    }
+} 
