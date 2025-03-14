5722b60e59c7b8213295c47d1a3ff6b60987a6e0
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-14 11:52:54 +0530

    Split the commas

---
+++ Split/Scase.java
@@ -0,0 +1,8 @@
+public class Scase{
+    public static void main(String args[])
+    {
+        String x="Hello,world";
+        String n[]=x.split(",");
+        System.out.println(n[1]);
+    }
+} 
