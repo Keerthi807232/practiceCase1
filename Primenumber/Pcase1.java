9601e734103d91c28ca944d0cc5cb6e10dfe0919
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-15 09:46:13 +0530

    Checking prime numbers

--- Primenumber/Pcase1.java
+++ Primenumber/Pcase1.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
-
-public class Pcase1 {
+public class Pcase1{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the value");
-        int n = input.nextInt();
-        if (n <= 1) {
+        int n=input.nextInt();
+        if (n<=1){
             System.out.println("it is not a prime number"+n);
         } 
-        else {
-            int count = 0;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    count++;
+        else{
+            int count=0;
+            for (int i=2;i<n;i++){
+                if (n%i==0) {
+                    count=count+1;
                     break; 
                 }
             }
-            if (count == 0) {
+            if(count==0){
                 System.out.println("It is a prime number"+n);
             } 
-            else {
+            else{
                 System.out.println("It is not a prime number"+n);
             }
         }
