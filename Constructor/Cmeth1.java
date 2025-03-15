842c25464645f02a4c460942eede70ba84ec308d
KeerthiP2006 <keerthi807232@gmail.com>
2025-03-15 10:38:30 +0530

    Constructor

---
+++ Constructor/Cmeth1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Cmeth1{
+    Cmeth1(){
+    System.out.println("Hello");
+    }
+    public static void main(String args[]){
+        Cmeth1 obj=new Cmeth1();
+        
+    }
+}
